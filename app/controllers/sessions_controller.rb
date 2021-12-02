d863035e9b6ceb9d313c2fb8b7349b8be5da941e
tuanbkc4 <tuanbachkhoadn@gmail.com>
2021-12-02 22:52:24 +0700

    Chapter 10: Show, edit, update user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,6 @@ class SessionsController < ApplicationController
   def active_user user
     log_in user
     params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-    redirect_to user
+    redirect_back_or user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :load_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-  end
+  def show; end
 
   def create
     @user = User.new user_params
@@ -18,7 +20,39 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @pagy, @users = pagy(User.all, items: Settings.length.items)
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = t "users.update.success"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    if user&.destroy
+      flash[:success] = t "users.destroy.success"
+    else
+      flash[:danger] = t "users.destroy.danger"
+    end
+    redirect_to users_url
+  end
+
   private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t "users.not_found"
+    redirect_to root_url
+  end
 
   def user_params
     params.require(:user).permit(
@@ -29,5 +63,18 @@ class UsersController < ApplicationController
       :birthday,
       :gender
     )
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.login.danger"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
